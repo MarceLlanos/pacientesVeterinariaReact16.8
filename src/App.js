2b139c31741b6cc3afa69277bf76affaac590d7b
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-27 17:10:32 -0400

    Se agrego los datos a storage

--- src/App.js
+++ src/App.js
@@ -5,53 +5,68 @@ import ListaCitas from './components/ListaCitas';
 
 class App extends Component {
 
-  state = { 
-    citas: []
-  }
+	state = { 
+		citas: []
+	}
 
-  crearNuevaCita = datos => {
-    // Copiar el state actual
-    const citas = [...this.state.citas, datos];
+	// Cuando la aplicacion carga
+	componentDidMount() {
+		const citasLS = localStorage.getItem('citas');
+		if(citasLS){
+			this.setState({
+				citas: JSON.parse(citasLS)
+			})
+		}
+	}
 
-    //agregar el nuevo state
-    this.setState({citas});
-  }
+	// Cuando eliminamos o agregamos una nueva cita
+	componentDidUpdate(prevProps, prevState) {
+		localStorage.setItem('citas', JSON.stringify(this.state.citas))
+	}
 
-  //Elimina las citas del state
-  eliminarCita = id => {
-    console.log(id)
-    // Tomar una copia del state
-    const citasActuales = {...this.state.citas};
+	crearNuevaCita = datos => {
+		// Copiar el state actual
+		const citas = [...this.state.citas, datos];
 
-    // Utilizar filter para sacar el elemento@id del arreglo
-    const citas = citasActuales.filter(cita => cita.id !== id);
+		//agregar el nuevo state
+		this.setState({citas});
+	}
 
-    //Actualizar el state
-    this.setState({citas})
-  }
+	//Elimina las citas del state
+	eliminarCita = id => {
+		console.log(id)
+		// Tomar una copia del state
+		const citasActuales = {...this.state.citas};
+
+		// Utilizar filter para sacar el elemento@id del arreglo
+		const citas = citasActuales.filter(cita => cita.id !== id);
+
+		//Actualizar el state
+		this.setState({citas})
+	}
 
 
-  render() { 
-    return (  
-      <div className="container">
-        <Header title ="Administrador Pacientes Veterinaria" />
+	render() { 
+		return (  
+			<div className="container">
+				<Header title ="Administrador Pacientes Veterinaria" />
 
-        <div className="row">
-          <div className="col-md-10 mx-auto">
-            <NuevaCita
-              crearNuevaCita = {this.crearNuevaCita}
-            />
-          </div>
-          <div className="mt-5 col-md-10 mx-auto">
-            <ListaCitas
-              citas = {this.state.citas}
-              eliminarCita = {this.eliminarCita}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+				<div className="row">
+					<div className="col-md-10 mx-auto">
+						<NuevaCita
+							crearNuevaCita = {this.crearNuevaCita}
+						/>
+					</div>
+					<div className="mt-5 col-md-10 mx-auto">
+						<ListaCitas
+							citas = {this.state.citas}
+							eliminarCita = {this.eliminarCita}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
  
 export default App;
